23e6b4b62eff5aa8da102fec30131433254f7ebd
Jose Campos <j.camposs2701@gmail.com>
2024-12-19 00:00:28 -0500

    Se incia el proyecto del crud con spriboot en java

---
+++ src/test/java/com/jose_campos/crud/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose_campos.crud.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
